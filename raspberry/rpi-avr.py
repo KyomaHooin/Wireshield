e5cdf305a40cdc906101c099ee0425df73da359d
KyomaHooin <richard_bruna@nm.cz>
2017-04-07 15:04:34 +0200

    Fix xonxoff flow control.

--- raspberry/rpi-avr.py
+++ raspberry/rpi-avr.py
@@ -8,7 +8,7 @@ import serial,time
 try:
 	while 1:# MAIN
 		try:# SERIAL
-			s = serial.Serial('/dev/ttyAMA0',9600,timeout=5)# 8,N,1; 5s scan..
+			s = serial.Serial('/dev/ttyAMA0',9600,xonxoff=0,timeout=5)# 8,N,1; 5s scan..
 			data = s.readline()
 			if data: print data
 		except serial.SerialException:
