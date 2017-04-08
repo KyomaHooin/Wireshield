80b8d7f76ef0d876128834455c58e0ca57ee5afb
KyomaHooin <richard_bruna@nm.cz>
2017-04-08 14:17:36 +0200

    Email notify.

--- raspberry/rpi-avr.py
+++ raspberry/rpi-avr.py
@@ -21,15 +21,32 @@
 #)
 #
 
-import rrdtool,serial,time,re
+import rrdtool,smtplib,serial,email,time,re
 
+from email.mime.text import MIMEText
+
+ADMIN='foo@bar.cz'
 DATABASE='/root/avr/rpishield.rrd'
 PLOT='/var/www/rpishield/plot/'
-
 PAYLOAD=''
 UPDATE=True
 
 #----------------
+
+
+def notify(s,t):
+	try:
+		text = "Sensor: " + s + "\n\nTemeperature: " + t + "\n\n"
+		msg = MIMEText(text)
+		msg['Subject'] = "Rack Sensor Alarm"
+		msg['From'] = 'root@xxx.xx.cz'
+		msg['To'] = ADMIN
+		s = smtplib.SMTP('xx.xx.cz')
+		s.sendmail('root@xxx.xx.xx', ADMIN, msg.as_string())
+		s.quit()
+	except: pass
+
+#----------------	
 
 try:
 	while 1:# MAIN
@@ -39,9 +56,13 @@ try:
 			if data: print data,
 	#		if data:
 	#			pattern = re.compile('^S1T(*)S2T(*)S3T(*)S4T(*)S5T(*)S6T(*)S7T(*)S8T(*)$')
+	#			value = re.compile('S(\d)T(\d{3})')
 	#			if re.match(pattern, data):
 	#				rrdtool.update('DATABASE', re.sub(pattern, time.gmtime()
 	#					+ ':\\1:\\2:\\3:\\4:\\5:\\6:\\7:\\8', data))
+	#			for (sid,val) in re.findall(value, data):
+	#				if value >= 40:# 40C
+	#					notify(sid,val)
 		except serial.SerialException:
 			print 'Serial error.'
 			time.sleep(5)
@@ -49,7 +70,7 @@ try:
 	#	if int(time.strftime("%M")) % 15 == 0 and UPDATE:
 	#		UPDATE=False
 	#		for interval in ['1d','1w','1m','1y']:			
-	#			rrdtool.graph(PLOT + interval +'.png',
+	#			rrdtool.graph(PLOT + 'rack_plot_' + ;interval +'.png',
 	#				'--title','NM Rack Temeprature',
 	#				'--start','now-' + interval,# substraction
 	#				'--end','now',
@@ -64,7 +85,7 @@ try:
 	#				'DEF:s6:' + DATABASE + 'S6:MAX',
 	#				'DEF:s7:' + DATABASE + 'S7:MAX',
 	#				'DEF:s8:' + DATABASE + 'S8:MAX',
-	#				..
+	#				...
 	#	#reset update token
 	#	if int(time.strftime("%M")) % 15 == 1: UPDATE=True
 except Exception as e:
