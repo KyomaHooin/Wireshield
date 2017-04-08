764b3e5841b0a13d4665ebd88ff0adc1b9a8d82a
KyomaHooin <richard_bruna@nm.cz>
2017-04-08 11:17:41 +0200

    RRD.

--- raspberry/rpi-avr.py
+++ raspberry/rpi-avr.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python
 #
-# AVR RX/TX GPIO
+# RPi AVR Shiled OneWire bus data processing
+#
+# DATA[5min]: S1T000S2T000S3T000S4T000S5T000S6T000S7T000S8T000
+#
+#rrdtool.create(DATABASE,
+#	"--step", "300", "--start", '0',# 5min update from now
+#	"DS:S1:GAUGE:400:-55:125",# min/max DS18B20 spec.
+#	"DS:S2:GAUGE:400:-55:125",
+#	"DS:S3:GAUGE:400:-55:125",
+#	"DS:S4:GAUGE:400:-55:125",
+#	"DS:S5:GAUGE:400:-55:125",
+#	"DS:S6:GAUGE:400:-55:125",
+#	"DS:S7:GAUGE:400:-55:125",
+#	"DS:S8:GAUGE:400:-55:125",
+#	"RRA:MAX:0.5:1:1d",	# 1-day "Roud Robin Archive"
+#	"RRA:MAX:0.5:3:1w",	# 1-week
+#	"RRA:MAX:0.5:12:1m",	# 1-month
+#	"RRA:MAX:0.5:72:1y",	# 1-year
+#)
 #
 
-import serial,time
+import rrdtool,serial,time,re
+
+DATABASE='/root/avr/rpishield.rrd'
+PLOT='/var/www/rpishield/plot/'
+
+PAYLOAD=''
+UPDATE=True
+
+#----------------
 
 try:
 	while 1:# MAIN
@@ -11,9 +37,36 @@ try:
 			s = serial.Serial('/dev/ttyAMA0',9600,xonxoff=0,timeout=5)# 8,N,1; 5s scan..
 			data = s.readline()
 			if data: print data,
+	#		if data:
+	#			pattern = re.compile('^S1T(*)S2T(*)S3T(*)S4T(*)S5T(*)S6T(*)S7T(*)S8T(*)$')
+	#			if re.match(pattern, data):
+	#				rrdtool.update('DATABASE', re.sub(pattern, time.gmtime()
+	#					+ ':\\1:\\2:\\3:\\4:\\5:\\6:\\7:\\8', data))
 		except serial.SerialException:
 			print 'Serial error.'
 			time.sleep(5)
+	#	#15 min graph plot update..
+	#	if int(time.strftime("%M")) % 15 == 0 and UPDATE:
+	#		UPDATE=False
+	#		for interval in ['1d','1w','1m','1y']:			
+	#			rrdtool.graph(PLOT + interval +'.png',
+	#				'--title','NM Rack Temeprature',
+	#				'--start','now-' + interval,# substraction
+	#				'--end','now',
+	#				'--width','600',
+	#				'--height','150',
+	#				'--step','60',
+	#				'DEF:s1=' + DATABASE + 'S1:MAX',
+	#				'DEF:s2=' + DATABASE + 'S2:MAX',
+	#				'DEF:s3=' + DATABASE + 'S3:MAX',
+	#				'DEF:s4=' + DATABASE + 'S4:MAX',
+	#				'DEF:s5:' + DATABASE + 'S5:MAX',
+	#				'DEF:s6:' + DATABASE + 'S6:MAX',
+	#				'DEF:s7:' + DATABASE + 'S7:MAX',
+	#				'DEF:s8:' + DATABASE + 'S8:MAX',
+	#				..
+	#	#reset update token
+	#	if int(time.strftime("%M")) % 15 == 1: UPDATE=True
 except Exception as e:
 	print e.args[0]
 
