a4d989362ba648b5631830ebc77e0f815b5137cb
KyomaHooin <richard_bruna@nm.cz>
2017-04-11 12:27:34 +0200

    RRD update, plot.

--- raspberry/rpi-avr.py
+++ raspberry/rpi-avr.py
@@ -4,24 +4,8 @@
 #
 # DATA[5min]: S1T000S2T000S3T000S4T000S5T000S6T000S7T000S8T000
 #
-#rrdtool.create(DATABASE,
-#	"--step", "300", "--start", '0',# 5min update from now
-#	"DS:S1:GAUGE:400:-55:125",# min/max DS18B20 spec.
-#	"DS:S2:GAUGE:400:-55:125",
-#	"DS:S3:GAUGE:400:-55:125",
-#	"DS:S4:GAUGE:400:-55:125",
-#	"DS:S5:GAUGE:400:-55:125",
-#	"DS:S6:GAUGE:400:-55:125",
-#	"DS:S7:GAUGE:400:-55:125",
-#	"DS:S8:GAUGE:400:-55:125",
-#	"RRA:MAX:0.5:1:1d",	# 1-day "Roud Robin Archive"
-#	"RRA:MAX:0.5:3:1w",	# 1-week
-#	"RRA:MAX:0.5:12:1m",	# 1-month
-#	"RRA:MAX:0.5:72:1y",	# 1-year
-#)
-#
 
-import rrdtool,smtplib,serial,email,time,re
+import rrdtool,smtplib,serial,email,time,sys,re
 
 from email.mime.text import MIMEText
 
@@ -32,7 +16,6 @@ PAYLOAD=''
 UPDATE=True
 
 #----------------
-
 
 def notify(s,t):
 	try:
@@ -46,57 +29,68 @@ def notify(s,t):
 		s.quit()
 	except: pass
 
-#----------------	
-
-#data = "S1T050S2T100S3T250S4T300S5T350S6T400S7T450S8T500"
+data = "S1T050S2T100S3T250S4T300S5T350S6T400S7T450S8T500"
 
 try:
+	try:
+		LOG = open('/var/log/rpishiled.log','a',0)
+		LOG.write('Program start: ' + time.strftime("%d.%m.%Y %H:%M") + '\n')
+	except IOError:
+		print 'Read only FS exiting...'
+		sys.exit(1)
 	while 1:# MAIN
-
 		try:# SERIAL
-			time.sleep(5)
-			s = serial.Serial('/dev/ttyAMA0',9600,xonxoff=0,timeout=5)# 8,N,1; 5s scan..
-			data = s.readline()
-
+			#s = serial.Serial('/dev/ttyAMA0',9600,xonxoff=0,timeout=5)# 8,N,1; 5s scan..
+			#data = s.readline()
 			if data:
-				print data,
 				pattern = re.compile('S.T(.*)S.T(.*)S.T(.*)S.T(.*)S.T(.*)S.T(.*)S.T(.*)S.T(.*)')
 				sensor = re.compile('S(.)T(...)')
 
 				if re.match(pattern,data):
-					print "update ok"
-				#	rrdtool.update(DATABASE, re.sub(pattern, time.gmtime() +
-				#		':\\1:\\2:\\3:\\4:\\5:\\6:\\7:\\8', data))
-				for (sid,val) in re.findall(sensor, data):
-					temperature = float(re.sub('(..)(.)','\\1.\\2',val))
-					if temperature > 40:
-						print sid, temperature
-					#	notify(sid,temperature)
-		except serial.SerialException:
-			print 'Serial error.'
-			time.sleep(5)
-	#	#15 min graph plot update..
-	#	if int(time.strftime("%M")) % 15 == 0 and UPDATE:
-	#		UPDATE=False
-	#		for interval in ['1d','1w','1m','1y']:			
-	#			rrdtool.graph(PLOT + 'rack_plot_' + ;interval +'.png',
-	#				'--title','NM Rack Temeprature',
-	#				'--start','now-' + interval,# substraction
-	#				'--end','now',
-	#				'--width','600',
-	#				'--height','150',
-	#				'--step','60',
-	#				'DEF:s1=' + DATABASE + 'S1:MAX',
-	#				'DEF:s2=' + DATABASE + 'S2:MAX',
-	#				'DEF:s3=' + DATABASE + 'S3:MAX',
-	#				'DEF:s4=' + DATABASE + 'S4:MAX',
-	#				'DEF:s5:' + DATABASE + 'S5:MAX',
-	#				'DEF:s6:' + DATABASE + 'S6:MAX',
-	#				'DEF:s7:' + DATABASE + 'S7:MAX',
-	#				'DEF:s8:' + DATABASE + 'S8:MAX',
-	#				...
-	#	#reset update token
-	#	if int(time.strftime("%M")) % 15 == 1: UPDATE=True
+					rrdtool.update(DATABASE, re.sub(pattern, 'N:' +
+						'\\1:\\2:\\3:\\4:\\5:\\6:\\7:\\8' , data))
+		#		for (sid,val) in re.findall(sensor, data):
+		#			temperature = float(re.sub('(\d\d)(\d)','\\1.\\2',val))
+		#			if temperature > 40:
+		#				print sid, temperature
+		#			#	notify(sid,temperature)
+			time.sleep(300)
+		#except serial.SerialException:
+		except:
+			LOG.write('Update error.\n')
+		#15 min graph plot update..
+		if int(time.strftime("%M")) % 15 == 0 and UPDATE:
+			UPDATE=False
+			try:
+				for interval in ['1d','1w','1m','1y']:			
+					rrdtool.graph(PLOT + 'rack_plot_' + interval +'.png',
+						'--title', 'NM Rack Temeprature',
+						'--start', 'now-' + interval,# substraction
+						'--end','now',
+						'--width','600',
+						'--height','150',
+						#'--step','60',
+						'DEF:s1=' + DATABASE + ':S1:MAX',
+						'DEF:s2=' + DATABASE + ':S2:MAX',
+						'DEF:s3=' + DATABASE + ':S3:MAX',
+						'DEF:s4=' + DATABASE + ':S4:MAX',
+						'DEF:s5=' + DATABASE + ':S5:MAX',
+						'DEF:s6=' + DATABASE + ':S6:MAX',
+						'DEF:s7=' + DATABASE + ':S7:MAX',
+						'DEF:s8=' + DATABASE + ':S8:MAX',
+						'LINE:s1#6959CD: S1',
+						'LINE:s2#00FF7F: S2',
+						'LINE:s3#EEEE00: S3',
+						'LINE:s4#EE7621: S4',
+						'LINE:s5#FF3030: S5',
+						'LINE:s6#EE799F: S6',
+						'LINE:s7#00CDCD: S7',
+						'LINE:s8#C0FF3E: S8',
+					)
+			except:
+				LOG.write('Plot error.\n')
+		#reset update token
+		if int(time.strftime("%M")) % 15 == 1: UPDATE=True
 except Exception as e:
 	print e.args[0]
 
