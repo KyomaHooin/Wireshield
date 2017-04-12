b7b27d61b3d5c7c8ce73c576959ee07923fa35ec
KyomaHooin <richard_bruna@nm.cz>
2017-04-12 18:07:02 +0200

    Temperature error.

--- avr/rpishield.ino
+++ avr/rpishield.ino
@@ -67,6 +67,7 @@ void loop() {
     digitalWrite(LED_ACC, HIGH);
     ds_temp_request(Bus1,sensor[i]);
     temperature = ds_get_temperature(Bus1,sensor[i]);
+    if ( temperature < 0 ) { temperature = 0; }
     strcat(payload,sprintf(msg, "S%uT%03d", i, int(temperature * 10)));
     tft_update(i,temperature);
     digitalWrite(LED_ACC, LOW);
@@ -75,6 +76,7 @@ void loop() {
     digitalWrite(LED_ACC, HIGH);
     ds_temp_request(Bus2,sensor[i]);
     temperature = ds_get_temperature(Bus2,sensor[i]);
+    if ( temperature < 0 ) { temperature = 0; }
     strcat(payload,sprintf(msg, "S%uT%03d", i, int(temperature * 10)));
     tft_update(i,temperature);
     digitalWrite(LED_ACC, LOW);
